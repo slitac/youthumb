5d49848446f8ba7bc769cbaca28057b90a83f4b9
slitac <tachroun.sliman@gmail.com>
2023-08-30 02:40:30 +0200

    adding

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,15 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N5ZNEQDY24"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N5ZNEQDY24');
+</script>
         </Head>
         <body>
           <Main />
