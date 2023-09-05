24587b122ab2ce87e1c355f28895514bd648876c
slitac <tachroun.sliman@gmail.com>
2023-09-05 12:52:54 +0200

    adding

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,8 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2596135861331646"
+     crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
